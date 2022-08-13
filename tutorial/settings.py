b80ea2b8fc86e9f125759086692d8cb726953da2
dyego <dyegoaurelio@gmail.com>
2022-08-13 15:14:22 -0300

    passando mypy

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-3ih^b0myrrvjr-b*v($j@ms_xa*&c7@1eg8gjo@6d5g34aofh1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
